d6520b9f5c2a16620ba387d8b3847dddfe76ddfc
大禹来敲门 <2777520544@qq.com>
2016-11-01 11:37:34 +0800

    修改项目的包名。

---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
@@ -0,0 +1,14 @@
+package cn.ucai.superwechat.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class GuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(cn.ucai.superwechat.R.layout.activity_guide);
+    }
+}
