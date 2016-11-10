7151230f7576ab9a51fb3a0cda979d7ff34894a5
大禹来敲门 <2777520544@qq.com>
2016-11-10 22:47:18 +0800

    在NetDao中添加两个创建群组的方法（一个可以添加头像，一个不可以），在NewGroupActivity中实现创建群组的方法（不上传头像的可以运行，上传头像的不可以运行）。

--- ChatDemoUI3.0/src/cn/ucai/superwechat/data/NetDao.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/data/NetDao.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 
 import com.hyphenate.chat.EMClient;
+import com.hyphenate.chat.EMGroup;
 
 import java.io.File;
 
@@ -19,6 +20,7 @@ public class NetDao {
 
     /**
      * 下载注册需要的数据。
+     *
      * @param context
      * @param username
      * @param usernick
@@ -28,8 +30,8 @@ public class NetDao {
     public static void register(Activity context, String username, String usernick, String userpassword, OkHttpUtils.OnCompleteListener<Result> listener) {
         OkHttpUtils<Result> utils = new OkHttpUtils<>(context);
         utils.setRequestUrl(I.REQUEST_REGISTER)
-                .addParam(I.User.USER_NAME,username)
-                .addParam(I.User.NICK,usernick)
+                .addParam(I.User.USER_NAME, username)
+                .addParam(I.User.NICK, usernick)
                 .addParam(I.User.PASSWORD, MD5.getMessageDigest(userpassword))
                 .post()
                 .targetClass(Result.class)
@@ -38,6 +40,7 @@ public class NetDao {
 
     /**
      * 取消注册
+     *
      * @param context
      * @param username
      * @param listener
@@ -45,7 +48,7 @@ public class NetDao {
     public static void unregister(Activity context, String username, OkHttpUtils.OnCompleteListener<Result> listener) {
         OkHttpUtils<Result> utils = new OkHttpUtils<>(context);
         utils.setRequestUrl(I.REQUEST_REGISTER)
-                .addParam(I.User.USER_NAME,username)
+                .addParam(I.User.USER_NAME, username)
                 .targetClass(Result.class)
                 .execute(listener);
     }
@@ -54,41 +57,44 @@ public class NetDao {
                              OkHttpUtils.OnCompleteListener<String> listener) {
         OkHttpUtils<String> utils = new OkHttpUtils<>(context);
         utils.setRequestUrl(I.REQUEST_LOGIN)
-                .addParam(I.User.USER_NAME,name)
-                .addParam(I.User.PASSWORD,MD5.getMessageDigest(password))
+                .addParam(I.User.USER_NAME, name)
+                .addParam(I.User.PASSWORD, MD5.getMessageDigest(password))
                 .targetClass(String.class)
                 .execute(listener);
     }
 
     /**
      * i下载更新昵称后的数据
+     *
      * @param context
      * @param name
      * @param nick
      * @param listener
      */
     public static void updatanick(Context context, String name, String nick,
-                             OkHttpUtils.OnCompleteListener<String> listener) {
+                                  OkHttpUtils.OnCompleteListener<String> listener) {
         OkHttpUtils<String> utils = new OkHttpUtils<>(context);
         utils.setRequestUrl(I.REQUEST_UPDATE_USER_NICK)
-                .addParam(I.User.USER_NAME,name)
-                .addParam(I.User.NICK,nick)
+                .addParam(I.User.USER_NAME, name)
+                .addParam(I.User.NICK, nick)
                 .targetClass(String.class)
                 .execute(listener);
     }
+
     /**
      * i下载更新头像后的数据
+     *
      * @param context
      * @param name
      * @param file
      * @param listener
      */
     public static void updateavatar(Context context, String name, File file,
-                                  OkHttpUtils.OnCompleteListener<String> listener) {
+                                    OkHttpUtils.OnCompleteListener<String> listener) {
         OkHttpUtils<String> utils = new OkHttpUtils<>(context);
         utils.setRequestUrl(I.REQUEST_UPDATE_AVATAR)
-                .addParam(I.NAME_OR_HXID,name)
-                .addParam(I.AVATAR_TYPE,I.AVATAR_TYPE_USER_PATH)
+                .addParam(I.NAME_OR_HXID, name)
+                .addParam(I.AVATAR_TYPE, I.AVATAR_TYPE_USER_PATH)
                 .addFile2(file)
                 .targetClass(String.class)
                 .post()
@@ -97,15 +103,16 @@ public class NetDao {
 
     /**
      * 下载收藏商品的数量
+     *
      * @param context
      * @param userName
      * @param listener
      */
     public static void syncUserInfo(Context context, String userName,
-                               OkHttpUtils.OnCompleteListener<String> listener) {
+                                    OkHttpUtils.OnCompleteListener<String> listener) {
         OkHttpUtils<String> utils = new OkHttpUtils<>(context);
         utils.setRequestUrl(I.REQUEST_FIND_USER)
-                .addParam(I.User.USER_NAME,userName)
+                .addParam(I.User.USER_NAME, userName)
                 .targetClass(String.class)
                 .execute(listener);
     }
@@ -113,43 +120,75 @@ public class NetDao {
 
     /**
      * 查找好友
+     *
      * @param context
      * @param name
      * @param listener
      */
     public static void searchFriend(Context context, String name,
-                             OkHttpUtils.OnCompleteListener<String> listener) {
+                                    OkHttpUtils.OnCompleteListener<String> listener) {
         OkHttpUtils<String> utils = new OkHttpUtils<>(context);
         utils.setRequestUrl(I.REQUEST_FIND_USER)
-                .addParam(I.User.USER_NAME,name)
+                .addParam(I.User.USER_NAME, name)
                 .targetClass(String.class)
                 .execute(listener);
     }
 
     public static void addContact(Context context, String name, String cususername,
-                             OkHttpUtils.OnCompleteListener<String> listener) {
-        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
-        utils.setRequestUrl(I.REQUEST_ADD_CONTACT)
-                .addParam(I.Contact.USER_NAME,name)
-                .addParam(I.Contact.CU_NAME,cususername)
-                .targetClass(String.class)
-                .execute(listener);
-    }
-    public static void delContact(Context context, String name, String cususername,
                                   OkHttpUtils.OnCompleteListener<String> listener) {
         OkHttpUtils<String> utils = new OkHttpUtils<>(context);
-        utils.setRequestUrl(I.REQUEST_DELETE_CONTACT)
-                .addParam(I.Contact.USER_NAME,name)
-                .addParam(I.Contact.CU_NAME,cususername)
+        utils.setRequestUrl(I.REQUEST_ADD_CONTACT)
+                .addParam(I.Contact.USER_NAME, name)
+                .addParam(I.Contact.CU_NAME, cususername)
                 .targetClass(String.class)
                 .execute(listener);
     }
 
-    public static void loadContact(Context context,OkHttpUtils.OnCompleteListener<String> listener) {
+    public static void delContact(Context context, String name, String cususername,
+                                  OkHttpUtils.OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_DELETE_CONTACT)
+                .addParam(I.Contact.USER_NAME, name)
+                .addParam(I.Contact.CU_NAME, cususername)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+    public static void loadContact(Context context, OkHttpUtils.OnCompleteListener<String> listener) {
         OkHttpUtils<String> utils = new OkHttpUtils<>(context);
         utils.setRequestUrl(I.REQUEST_DOWNLOAD_CONTACT_ALL_LIST)
                 .addParam(I.Contact.USER_NAME, EMClient.getInstance().getCurrentUser())
                 .targetClass(String.class)
                 .execute(listener);
+    }
+
+    public static void createGroup(Context context, EMGroup emGroup, OkHttpUtils.OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_CREATE_GROUP)
+                .addParam(I.Group.HX_ID, emGroup.getGroupId())
+                .addParam(I.Group.NAME, emGroup.getGroupName())
+                .addParam(I.Group.DESCRIPTION, emGroup.getDescription())
+                .addParam(I.Group.OWNER, emGroup.getOwner())
+                .addParam(I.Group.IS_PUBLIC, String.valueOf(emGroup.isPublic()))
+                .addParam(I.Group.ALLOW_INVITES, String.valueOf(emGroup.isAllowInvites()))
+                .targetClass(String.class)
+                .post()
+                .execute(listener);
+    }
+
+    public static void createGroup(Context context, EMGroup emGroup, File file, OkHttpUtils.OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_CREATE_GROUP)
+                .addParam(I.Group.HX_ID, emGroup.getGroupId())
+                .addParam(I.Group.NAME, emGroup.getGroupName())
+                .addParam(I.Group.DESCRIPTION, emGroup.getDescription())
+                .addParam(I.Group.OWNER, emGroup.getOwner())
+                .addParam(I.Group.IS_PUBLIC, String.valueOf(emGroup.isPublic()))
+                .addParam(I.Group.ALLOW_INVITES, String.valueOf(emGroup.isAllowInvites()))
+                .targetClass(String.class)
+                .addFile2(file)
+                .post()
+                .execute(listener);
+
     }
 }
--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/NewGroupActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/NewGroupActivity.java
@@ -13,15 +13,26 @@
  */
 package cn.ucai.superwechat.ui;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,10 +41,21 @@ import com.hyphenate.chat.EMGroup;
 import com.hyphenate.chat.EMGroupManager.EMGroupOptions;
 import com.hyphenate.chat.EMGroupManager.EMGroupStyle;
 
+import cn.hyphenate.easeui.domain.Group;
+import cn.hyphenate.easeui.utils.EaseImageUtils;
 import cn.hyphenate.easeui.widget.EaseAlertDialog;
+import cn.ucai.superwechat.I;
+import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.bean.Result;
 import cn.ucai.superwechat.data.NetDao;
+import cn.ucai.superwechat.data.OkHttpUtils;
+import cn.ucai.superwechat.utils.ResultUtils;
 
 import com.hyphenate.exceptions.HyphenateException;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class NewGroupActivity extends BaseActivity {
 	private EditText groupNameEditText;
@@ -42,6 +64,15 @@ public class NewGroupActivity extends BaseActivity {
 	private CheckBox publibCheckBox;
 	private CheckBox memberCheckbox;
 	private TextView secondTextView;
+	private LinearLayout layoutgroupavatar;
+	private ImageView mivGroupavatar;
+	private TextView mnewgroupsave;
+
+	private static final int REQUESTCODE_PICK = 1;
+	private static final int REQUESTCODE_CUTTING = 2;
+	private static final int REQUESTCODE_PICK_MEMBER = 3;
+
+	File avatarFile = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -49,17 +80,33 @@ public class NewGroupActivity extends BaseActivity {
 		setContentView(cn.ucai.superwechat.R.layout.em_activity_new_group);
 
 		initView();
+		setListener();
 
+	}
+
+	private void setListener() {
 		publibCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
-		    @Override
-		    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-		        if(isChecked){
-		            secondTextView.setText(cn.ucai.superwechat.R.string.join_need_owner_approval);
-		        }else{
-                    secondTextView.setText(cn.ucai.superwechat.R.string.Open_group_members_invited);
-		        }
-		    }
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if(isChecked){
+					secondTextView.setText(cn.ucai.superwechat.R.string.join_need_owner_approval);
+				}else{
+					secondTextView.setText(cn.ucai.superwechat.R.string.Open_group_members_invited);
+				}
+			}
+		});
+		layoutgroupavatar.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				uploadHeadPhoto();
+			}
+		});
+		mnewgroupsave.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				save();
+			}
 		});
 	}
 
@@ -69,6 +116,9 @@ public class NewGroupActivity extends BaseActivity {
 		publibCheckBox = (CheckBox) findViewById(cn.ucai.superwechat.R.id.cb_public);
 		memberCheckbox = (CheckBox) findViewById(cn.ucai.superwechat.R.id.cb_member_inviter);
 		secondTextView = (TextView) findViewById(cn.ucai.superwechat.R.id.second_desc);
+		layoutgroupavatar = (LinearLayout) findViewById(R.id.layout_group_avatar);
+		mivGroupavatar = (ImageView) findViewById(R.id.ivGroup_avatar);
+		mnewgroupsave = (TextView) findViewById(R.id.new_group_save);
 	}
 
 	public void save() {
@@ -77,63 +127,207 @@ public class NewGroupActivity extends BaseActivity {
 		    new EaseAlertDialog(this, cn.ucai.superwechat.R.string.Group_name_cannot_be_empty).show();
 		} else {
 			// select from contact list
-			startActivityForResult(new Intent(this, GroupPickContactsActivity.class).putExtra("groupName", name), 0);
+			startActivityForResult(new Intent(this, GroupPickContactsActivity.class).putExtra("groupName", name), REQUESTCODE_PICK_MEMBER);
 		}
 	}
 	
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
+
+		switch (requestCode) {
+			case REQUESTCODE_PICK:
+				if (data == null || data.getData() == null) {
+					return;
+				}
+				startPhotoZoom(data.getData());
+				break;
+			case REQUESTCODE_CUTTING:
+				if (data != null) {
+//					updateAppUserAvatar(data);
+					setPicToView(data);
+				}
+				break;
+			case REQUESTCODE_PICK_MEMBER:
+				if (resultCode == RESULT_OK) {
+					createEMGroup(data);
+				}
+				break;
+			default:
+				break;
+		}
 		super.onActivityResult(requestCode, resultCode, data);
+		if (resultCode == RESULT_OK) {
+			createEMGroup(data);
+		}
+	}
+
+	private void createEMGroup( final Intent data) {
 		String st1 = getResources().getString(cn.ucai.superwechat.R.string.Is_to_create_a_group_chat);
 		final String st2 = getResources().getString(cn.ucai.superwechat.R.string.Failed_to_create_groups);
-		if (resultCode == RESULT_OK) {
-			//new group
-			progressDialog = new ProgressDialog(this);
-			progressDialog.setMessage(st1);
-			progressDialog.setCanceledOnTouchOutside(false);
-			progressDialog.show();
+		//new group
+		progressDialog = new ProgressDialog(this);
+		progressDialog.setMessage(st1);
+		progressDialog.setCanceledOnTouchOutside(false);
+		progressDialog.show();
 
-			new Thread(new Runnable() {
-				@Override
-				public void run() {
-					final String groupName = groupNameEditText.getText().toString().trim();
-					String desc = introductionEditText.getText().toString();
-					String[] members = data.getStringArrayExtra("newmembers");
-					try {
-						EMGroupOptions option = new EMGroupOptions();
-					    option.maxUsers = 200;
-					    
-					    String reason = NewGroupActivity.this.getString(cn.ucai.superwechat.R.string.invite_join_group);
-					    reason  = EMClient.getInstance().getCurrentUser() + reason + groupName;
-					    
-						if(publibCheckBox.isChecked()){
-						    option.style = memberCheckbox.isChecked() ? EMGroupStyle.EMGroupStylePublicJoinNeedApproval : EMGroupStyle.EMGroupStylePublicOpenJoin;
-						}else{
-						    option.style = memberCheckbox.isChecked()?EMGroupStyle.EMGroupStylePrivateMemberCanInvite:EMGroupStyle.EMGroupStylePrivateOnlyOwnerInvite;
-						}
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				final String groupName = groupNameEditText.getText().toString().trim();
+				String desc = introductionEditText.getText().toString();
+				String[] members = data.getStringArrayExtra("newmembers");
+				try {
+					EMGroupOptions option = new EMGroupOptions();
+					option.maxUsers = 200;
 
+					String reason = NewGroupActivity.this.getString(cn.ucai.superwechat.R.string.invite_join_group);
+					reason  = EMClient.getInstance().getCurrentUser() + reason + groupName;
 
-						EMGroup emGroup = EMClient.getInstance().groupManager().createGroup(groupName, desc, members, reason, option);
-						emGroup.getGroupId();
-
-						runOnUiThread(new Runnable() {
-							public void run() {
-								progressDialog.dismiss();
-								setResult(RESULT_OK);
-								finish();
-							}
-						});
-					} catch (final HyphenateException e) {
-						runOnUiThread(new Runnable() {
-							public void run() {
-								progressDialog.dismiss();
-								Toast.makeText(NewGroupActivity.this, st2 + e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
-							}
-						});
+					if(publibCheckBox.isChecked()){
+						option.style = memberCheckbox.isChecked() ? EMGroupStyle.EMGroupStylePublicJoinNeedApproval : EMGroupStyle.EMGroupStylePublicOpenJoin;
+					}else{
+						option.style = memberCheckbox.isChecked()?EMGroupStyle.EMGroupStylePrivateMemberCanInvite:EMGroupStyle.EMGroupStylePrivateOnlyOwnerInvite;
 					}
-					
+
+
+					EMGroup emGroup = EMClient.getInstance().groupManager().createGroup(groupName, desc, members, reason, option);
+					emGroup.getGroupId();
+
+					createAppGroup(emGroup);
+				} catch (final HyphenateException e) {
+					runOnUiThread(new Runnable() {
+						public void run() {
+							progressDialog.dismiss();
+							Toast.makeText(NewGroupActivity.this, st2 + e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+						}
+					});
 				}
-			}).start();
+
+			}
+		}).start();
+	}
+
+	private void createAppGroup(EMGroup emGroup) {
+		if (avatarFile == null) {
+			NetDao.createGroup(this, emGroup, new OkHttpUtils.OnCompleteListener<String>() {
+				@Override
+				public void onSuccess(String s) {
+						afterCreateAppGroup(s);
+				}
+
+				@Override
+				public void onError(String error) {
+
+				}
+			});
+		} else {
+			NetDao.createGroup(this, emGroup, avatarFile, new OkHttpUtils.OnCompleteListener<String>() {
+				@Override
+				public void onSuccess(String s) {
+					afterCreateAppGroup(s);
+				}
+
+				@Override
+				public void onError(String error) {
+
+				}
+			});
+		}
+	}
+
+	private void afterCreateAppGroup(String s) {
+		if (s != null) {
+			Result result = ResultUtils.getResultFromJson(s, Group.class);
+			if (result != null && result.isRetMsg()) {
+				Group group = (Group) result.getRetData();
+				createGroupSuccess();
+			}
+		}
+	}
+
+
+	private void createGroupSuccess() {
+		runOnUiThread(new Runnable() {
+			public void run() {
+				progressDialog.dismiss();
+				setResult(RESULT_OK);
+				finish();
+			}
+		});
+		progressDialog.dismiss();
+
+	}
+
+	private void uploadHeadPhoto() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle(cn.ucai.superwechat.R.string.dl_title_upload_photo);
+		builder.setItems(new String[] { getString(cn.ucai.superwechat.R.string.dl_msg_take_photo), getString(cn.ucai.superwechat.R.string.dl_msg_local_upload) },
+				new DialogInterface.OnClickListener() {
+
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.dismiss();
+						switch (which) {
+							case 0:
+								Toast.makeText(NewGroupActivity.this, getString(cn.ucai.superwechat.R.string.toast_no_support),
+										Toast.LENGTH_SHORT).show();
+								break;
+							case 1:
+								Intent pickIntent = new Intent(Intent.ACTION_PICK,null);
+								pickIntent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+								startActivityForResult(pickIntent, REQUESTCODE_PICK);
+								break;
+							default:
+								break;
+						}
+					}
+				});
+		builder.create().show();
+	}
+
+	public void startPhotoZoom(Uri uri) {
+		Intent intent = new Intent("com.android.camera.action.CROP");
+		intent.setDataAndType(uri, "image/*");
+		intent.putExtra("crop", true);
+		intent.putExtra("aspectX", 1);
+		intent.putExtra("aspectY", 1);
+		intent.putExtra("outputX", 300);
+		intent.putExtra("outputY", 300);
+		intent.putExtra("return-data", true);
+		intent.putExtra("noFaceDetection", true);
+		startActivityForResult(intent, REQUESTCODE_CUTTING);
+	}
+
+	/**
+	 * save the picture data
+	 *
+	 * @param picdata
+	 */
+	private void setPicToView(Intent picdata) {
+		Bundle extras = picdata.getExtras();
+		if (extras != null) {
+			Bitmap photo = extras.getParcelable("data");
+			Drawable drawable = new BitmapDrawable(getResources(), photo);
+			mivGroupavatar.setImageDrawable(drawable);
+			saveBitmapFile(picdata);
+		}
+
+	}
+
+	public void saveBitmapFile(Intent picavatar) {
+		Bundle extras = picavatar.getExtras();
+		if (extras != null) {
+			Bitmap bitmap = extras.getParcelable("data");
+			String imagepath = EaseImageUtils.getImagePath(System.currentTimeMillis()+ I.AVATAR_SUFFIX_JPG);
+			File file = new File(imagepath);
+			try {
+				BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+				bitmap.compress(Bitmap.CompressFormat.PNG, 100, bos);
+				bos.flush();
+				bos.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			avatarFile =  file;
 		}
 	}
 
