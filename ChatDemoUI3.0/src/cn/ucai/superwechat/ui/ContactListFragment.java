a43e5740c7ed5bfa10e4d9a07c2bb9a22b53dc2a
大禹来敲门 <2777520544@qq.com>
2016-11-10 09:48:15 +0800

    修改EaseUI中的EaseUser为User。

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/ContactListFragment.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/ContactListFragment.java
@@ -18,6 +18,7 @@ import java.util.Map;
 
 import com.hyphenate.chat.EMClient;
 
+import cn.hyphenate.easeui.domain.EaseUser;
 import cn.hyphenate.easeui.domain.User;
 import cn.ucai.superwechat.SuperWeChatHelper;
 import cn.ucai.superwechat.SuperWeChatHelper.DataSyncListener;
@@ -31,7 +32,7 @@ import cn.ucai.superwechat.utils.L;
 import cn.ucai.superwechat.utils.MFGT;
 import cn.ucai.superwechat.utils.ResultUtils;
 import cn.ucai.superwechat.widget.ContactItemView;
-import cn.hyphenate.easeui.domain.EaseUser;
+import cn.hyphenate.easeui.domain.User;
 import cn.hyphenate.easeui.ui.EaseContactListFragment;
 
 import com.hyphenate.util.EMLog;
@@ -86,10 +87,10 @@ public class ContactListFragment extends EaseContactListFragment {
     
     @Override
     public void refresh() {
-        Map<String, EaseUser> m = SuperWeChatHelper.getInstance().getContactList();
+        Map<String, User> m = SuperWeChatHelper.getInstance().getAppContactList();
         if (m instanceof Hashtable<?, ?>) {
             //noinspection unchecked
-            m = (Map<String, EaseUser>) ((Hashtable<String, EaseUser>)m).clone();
+            m = (Map<String, User>) ((Hashtable<String, User>)m).clone();
         }
         setContactsMap(m);
         super.refresh();
@@ -117,9 +118,9 @@ public class ContactListFragment extends EaseContactListFragment {
             }
         });
         //设置联系人数据
-        Map<String, EaseUser> m = SuperWeChatHelper.getInstance().getContactList();
+        Map<String, User> m = SuperWeChatHelper.getInstance().getAppContactList();
         if (m instanceof Hashtable<?, ?>) {
-            m = (Map<String, EaseUser>) ((Hashtable<String, EaseUser>)m).clone();
+            m = (Map<String, User>) ((Hashtable<String, User>)m).clone();
         }
         setContactsMap(m);
         super.setUpView();
@@ -127,9 +128,9 @@ public class ContactListFragment extends EaseContactListFragment {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                EaseUser user = (EaseUser)listView.getItemAtPosition(position);
+                User user = (User)listView.getItemAtPosition(position);
                 if (user != null) {
-                    String username = user.getUsername();
+                    String username = user.getMUserName();
                     // demo中直接进入聊天页面，实际一般是进入用户详情页
                     L.e(TAG,"username = "+username);
                     L.e(TAG,"user = "+SuperWeChatHelper.getInstance().getAppContactList().get(username));
@@ -218,8 +219,8 @@ public class ContactListFragment extends EaseContactListFragment {
 	@Override
 	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
 		super.onCreateContextMenu(menu, v, menuInfo);
-	    toBeProcessUser = (EaseUser) listView.getItemAtPosition(((AdapterContextMenuInfo) menuInfo).position);
-	    toBeProcessUsername = toBeProcessUser.getUsername();
+	    toBeProcessUser = (User) listView.getItemAtPosition(((AdapterContextMenuInfo) menuInfo).position);
+	    toBeProcessUsername = toBeProcessUser.getMUserName();
 		getActivity().getMenuInflater().inflate(cn.ucai.superwechat.R.menu.em_context_contact_list, menu);
 	}
 
@@ -231,7 +232,7 @@ public class ContactListFragment extends EaseContactListFragment {
                 deleteContact(toBeProcessUser);
                 // remove invitation message
                 InviteMessgeDao dao = new InviteMessgeDao(getActivity());
-                dao.deleteMessage(toBeProcessUser.getUsername());
+                dao.deleteMessage(toBeProcessUser.getMUserName());
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -250,20 +251,20 @@ public class ContactListFragment extends EaseContactListFragment {
 	 * 
 	 * @param tobeDeleteUser
 	 */
-	public void deleteContact(final EaseUser tobeDeleteUser) {
+	public void deleteContact(final User tobeDeleteUser) {
 		String st1 = getResources().getString(cn.ucai.superwechat.R.string.deleting);
 		final String st2 = getResources().getString(cn.ucai.superwechat.R.string.Delete_failed);
 		final ProgressDialog pd = new ProgressDialog(getActivity());
 		pd.setMessage(st1);
 		pd.setCanceledOnTouchOutside(false);
 		pd.show();
-        NetDao.delContact(getActivity(), EMClient.getInstance().getCurrentUser(), tobeDeleteUser.getUsername(), new OkHttpUtils.OnCompleteListener<String>() {
+        NetDao.delContact(getActivity(), EMClient.getInstance().getCurrentUser(), tobeDeleteUser.getMUserName(), new OkHttpUtils.OnCompleteListener<String>() {
             @Override
             public void onSuccess(String s) {
                 if (s != null) {
                     Result result = ResultUtils.getResultFromJson(s, User.class);
                     if (result != null && result.isRetMsg()) {
-                        SuperWeChatHelper.getInstance().delAppContact(tobeDeleteUser.getUsername());
+                        SuperWeChatHelper.getInstance().delAppContact(tobeDeleteUser.getMUserName());
                     }
                 }
             }
@@ -276,11 +277,11 @@ public class ContactListFragment extends EaseContactListFragment {
 		new Thread(new Runnable() {
 			public void run() {
 				try {
-					EMClient.getInstance().contactManager().deleteContact(tobeDeleteUser.getUsername());
+					EMClient.getInstance().contactManager().deleteContact(tobeDeleteUser.getMUserName());
 					// remove user from memory and database
 					UserDao dao = new UserDao(getActivity());
-					dao.deleteContact(tobeDeleteUser.getUsername());
-					SuperWeChatHelper.getInstance().getContactList().remove(tobeDeleteUser.getUsername());
+					dao.deleteContact(tobeDeleteUser.getMUserName());
+					SuperWeChatHelper.getInstance().getContactList().remove(tobeDeleteUser.getMUserName());
 					getActivity().runOnUiThread(new Runnable() {
 						public void run() {
 							pd.dismiss();
--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/ForwardMessageActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/ForwardMessageActivity.java
@@ -16,11 +16,11 @@ package cn.ucai.superwechat.ui;
 import android.content.Intent;
 import android.os.Bundle;
 
-import cn.hyphenate.easeui.domain.EaseUser;
+import cn.hyphenate.easeui.domain.User;
 import cn.hyphenate.easeui.widget.EaseAlertDialog;
 
 public class ForwardMessageActivity extends PickContactNoCheckboxActivity {
-	private EaseUser selectUser;
+	private User selectUser;
 	private String forward_msg_id;
 
 	 
@@ -33,7 +33,7 @@ public class ForwardMessageActivity extends PickContactNoCheckboxActivity {
 	@Override
 	protected void onListItemClick(int position) {
 		selectUser = contactAdapter.getItem(position);
-		new EaseAlertDialog(this, null, getString(cn.ucai.superwechat.R.string.confirm_forward_to, selectUser.getNick()), null, new EaseAlertDialog.AlertDialogUser() {
+		new EaseAlertDialog(this, null, getString(cn.ucai.superwechat.R.string.confirm_forward_to, selectUser.getMUserNick()), null, new EaseAlertDialog.AlertDialogUser() {
             @Override
             public void onResult(boolean confirmed, Bundle bundle) {
                 if (confirmed) {
@@ -45,7 +45,7 @@ public class ForwardMessageActivity extends PickContactNoCheckboxActivity {
                     }
                     Intent intent = new Intent(ForwardMessageActivity.this, ChatActivity.class);
                     // it is single chat
-                    intent.putExtra("userId", selectUser.getUsername());
+                    intent.putExtra("userId", selectUser.getMUserName());
                     intent.putExtra("forward_msg_id", forward_msg_id);
                     startActivity(intent);
                     finish();
